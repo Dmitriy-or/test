c2ba2ce9e5b47445b5d6c58a5f580da4f7df0ce6
Dmitriy <dmitriy-or@mail.ru>
2020-03-08 05:12:35 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+var aa="Yeoman";
+console.log(aa);
+//alert('ytllo');
+//confirm('here?');
+//let answer = prompt('18+?','yea');
+//console.log(typeof(answer));
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
